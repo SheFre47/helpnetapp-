40c20ec0ece8bd5c3121d3054630b91c52e485ed
SheFre47 <whx3lse@gmail.com>
2025-06-02 22:53:45 +0200

    Update MapComponent.js

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -1,30 +1,18 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
 
-// Fix for marker icons not showing correctly
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
-  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
-});
-
-const MapComponent = () => {
+function MapComponent() {
   return (
-    <MapContainer center={[0.3476, 32.5825]} zoom={13} style={{ height: '400px', width: '100%' }}>
+    <MapContainer
+      center={[0, 0]}
+      zoom={2}
+      style={{ height: '100vh', width: '100%' }}
+    >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[0.3476, 32.5825]}>
-        <Popup>
-          Example location (e.g., Kampala, Uganda).
-        </Popup>
-      </Marker>
     </MapContainer>
   );
-};
+}
 
 export default MapComponent;
