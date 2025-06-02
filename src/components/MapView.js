295b57dec1ae346a5d8e6aa2dd0f62bb81b72023
SheFre47 <whx3lse@gmail.com>
2025-06-02 19:37:23 +0200

    Add MapView with Leaflet

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,1 +1,34 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
+// Fix default marker icon path issue
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
+
+const MapView = () => {
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <MapContainer center={[0, 0]} zoom={2} style={{ height: "100%", width: "100%" }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[0, 0]}>
+          <Popup>You are here</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapView;
