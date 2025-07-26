aaf0630d1cf2a51acfd2b2bd7d8e938d6be03420
SheFre47 <whx3lse@gmail.com>
2025-07-26 15:44:00 +0200

    Update MapComponent.js

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -3,34 +3,31 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
-// Fix for missing marker icons in Leaflet on some devices
 delete L.Icon.Default.prototype._getIconUrl;
+
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
-  iconUrl:
-    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-  shadowUrl:
-    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
 });
 
-const MapComponent = ({ helpRequests }) => {
+function MapComponent({ helpRequests }) {
   return (
-    <MapContainer center={[0, 0]} zoom={2} style={{ height: '400px', width: '100%' }}>
+    <MapContainer center={[-26.2041, 28.0473]} zoom={11} style={{ height: '500px', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; OpenStreetMap contributors"
       />
-      {helpRequests.map((request) => (
-        <Marker key={request.id} position={request.position}>
+      {helpRequests.map((req, index) => (
+        <Marker key={index} position={[req.latitude, req.longitude]}>
           <Popup>
-            <strong>{request.name}</strong><br />
-            {request.description}
+            <strong>{req.name}</strong><br />
+            {req.message}
           </Popup>
         </Marker>
       ))}
     </MapContainer>
   );
-};
+}
 
 export default MapComponent;
