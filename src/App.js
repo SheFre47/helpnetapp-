a91307b7504e19ffd663789881de775abc82344e
SheFre47 <whx3lse@gmail.com>
2025-07-26 21:27:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
-import React, { useState } from 'react';
-import HelpRequestForm from './HelpRequestForm';
-import MapComponent from './MapComponent';
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import HelpRequestForm from "./HelpRequestForm";
 
-function App() {
-  const [helpRequests, setHelpRequests] = useState([]);
+// Fix default marker icon issue
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
 
-  const handleAddRequest = (newRequest) => {
-    setHelpRequests((prev) => [...prev, newRequest]);
+const App = () => {
+  const [requests, setRequests] = useState([]);
+
+  const addRequest = (newRequest) => {
+    setRequests((prev) => [...prev, newRequest]);
   };
 
   return (
     <div>
-      <HelpRequestForm onAddRequest={handleAddRequest} />
-      <MapComponent helpRequests={helpRequests} />
+      <HelpRequestForm onAddRequest={addRequest} />
+
+      <MapContainer
+        center={[-26.2041, 28.0473]} // Johannesburg default
+        zoom={10}
+        style={{ height: "500px", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; OpenStreetMap contributors"
+        />
+
+        {requests.map((req) => (
+          <Marker key={req.id} position={req.position}>
+            <Popup>
+              <strong>{req.name}</strong>
+              <br />
+              {req.description}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
-}
+};
 
 export default App;
