984d163e1b6ebc8af4b713464f29936b88ec6da4
SheFre47 <whx3lse@gmail.com>
2025-07-29 19:39:29 +0200

    Update MapComponent.js

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -4,46 +4,43 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
-const customIcon = new L.Icon({
-  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
+// Fix default Leaflet icon paths
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
 });
 
 export default function MapComponent() {
-  const [helpRequests, setHelpRequests] = useState([]);
+  const [requests, setRequests] = useState([]);
 
   useEffect(() => {
     fetch('/api/getHelpRequests')
       .then(res => res.json())
-      .then(data => {
-        if (data.success) {
-          setHelpRequests(data.data);
-        }
+      .then(json => {
+        if (json.success) setRequests(json.data);
       })
-      .catch(err => console.error('Error loading help requests:', err));
+      .catch(console.error);
   }, []);
 
   return (
-    <MapContainer center={[-26.2041, 28.0473]} zoom={10} style={{ height: '500px', width: '100%' }}>
+    <MapContainer center={[0, 0]} zoom={2} style={{ height: '100vh', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; OpenStreetMap contributors"
       />
-
-      {helpRequests.map((request, index) => (
+      {requests.map((req) => (
         <Marker
-          key={index}
-          position={[request.latitude, request.longitude]}
-          icon={customIcon}
+          key={req._id}
+          position={[req.location.lat, req.location.lng]}
         >
           <Popup>
-            <strong>{request.name}</strong><br />
-            {request.description}
+            <strong>{req.name}</strong><br />
+            {req.description}
           </Popup>
         </Marker>
       ))}
     </MapContainer>
   );
-                                                     }
+            }
