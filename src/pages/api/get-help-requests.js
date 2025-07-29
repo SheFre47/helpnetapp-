33e6d046d34487de3a2d758ca6e6a2f471c68a0c
SheFre47 <whx3lse@gmail.com>
2025-07-29 22:24:19 +0200

    Delete src/pages directory

--- src/pages/api/get-help-requests.js
+++
@@ -1,9 +0,0 @@
-// src/pages/api/get-help-requests.js
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).json({ success: true, data: [] });
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end('Method Not Allowed');
-  }
-}
